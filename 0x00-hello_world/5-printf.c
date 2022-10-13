30e4f9f889c097650fc6f20520b71ff07e563f02
Dukula <dutandrew78@gmail.com>
2022-10-13 11:21:38 -0700

    Output using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Entry path for all programs
+* Return: returns zero when program runs successfully
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
