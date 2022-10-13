991d1c767dcb5f04eea28b027338e704945d3b40
Dukula <dutandrew78@gmail.com>
2022-10-13 11:14:37 -0700

    Output using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Entry path for all programs
+* Return: returns zero when program runs successfully
+*/
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
