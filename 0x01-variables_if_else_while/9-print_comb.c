ca07d47f06230916c5e9b4091a13832a933674e1
Dukula <dutandrew78@gmail.com>
2022-10-15 05:27:02 -0700

    print decimal numbers separated by comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry path
+ * Return: ) when successful
+*/
+
+int main(void)
+{
+/**
+ * all decimal numbers are represented in ASII code from 48 - 57
+ * in ASCII a comma is 44
+*/
+	int nums;
+
+	for (nums = 48; nums <=57; nums++)
+	{
+		putchar(nums);
+		putchar(44);
+	}
+		putchar(10);
+	return (0);
+}
