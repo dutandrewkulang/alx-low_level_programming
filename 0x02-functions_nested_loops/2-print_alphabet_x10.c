1360da7776983e75f3ccfd6bb2b6a3d8ae6a1ce0
Dukula <dutandrew78@gmail.com>
2022-10-19 14:41:08 -0700

    a-z 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints out a-z
+ * Return: void
+ * There are more than two ways to solve thgis question
+*/
+
+void print_alphabet(void)
+{
+	char alpha;
+	int count;
+	while (count == 0; count <= 10; count++)
+		{
+			for (alpha = 'a'; alpha <= 'z'; alpha++)
+				{
+					_putchar(alpha);
+				}
+			_putchar(10);
+		}
+}
