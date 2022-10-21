98519c9e8d1fd006fc2d19712a7a80a64883bfe8
Dukula <dutandrew78@gmail.com>
2022-10-21 17:30:33 +0300

    print numbers from 0 through 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,10 @@
 */
 void print_numbers(void)
 {
-  int a;
-  
-  for (a = 48; a <= 57; a++)
-    {
-      _putchar(a);
-    }
-  _putchar(10);
+int a;
+for (a = 48; a <= 57; a++)
+{
+_putchar(a);
+}
+_putchar(10);
 }
