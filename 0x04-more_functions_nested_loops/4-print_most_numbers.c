4f7da9e7d2cc91dcd80147e6737c15ac8120c960
Dukula <dutandrew78@gmail.com>
2022-10-21 23:03:38 +0300

    print all numbers from 1 to 9 a part 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,21 @@
 #include "main.h"
 /**
  * print_more_numbers - prints out all numbers from 0 -> 9 a part from 2 and 4
- * Return: void
- * Checks if character is upper case
+ * Return: All numbers a part from 2 and 4
+ * does not print 2 and 4
 */
-void print_more_numbers(void)
+
+void print_most_numbers(void)
 {
-int a;
-for (a = 48; a <= 57; a++)
-{
-if (a != 50 && a != 52)
-{
-_putchar(a);
-}
-}
-_putchar(10);
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if (a != 50 && a != 52)
+		{
+			_putchar(a);
+		}
+	}
+
+	_putchar(10);
 }
