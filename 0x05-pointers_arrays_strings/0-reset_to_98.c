e267f4ccfa13d02a65e9ed6aae718730463449ab
Dukula <dutandrew78@gmail.com>
2022-10-24 18:33:04 +0300

    rest back to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,11 @@
 #include "main.h"
-
-void reset_to_98(x)
+/**
+ * reset_to_98 - resets the value at the memory address at parameter address
+ * and replaces it will 98
+ * @p: is a pointer variable
+*/
+void reset_to_98(int *p)
 {
-
+	*p = 98;
 
 }
