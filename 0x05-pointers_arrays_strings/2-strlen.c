692750598b0a9ef65ea52b3af6b2d1ed1b250857
Dukula <dutandrew78@gmail.com>
2022-10-24 19:13:32 +0300

    Length of characters

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strlen - takes in a string of characters and return the string length
+ * @s: input parameter
+ * return: the integer length of string of characters in s
+ */
+int _strlen(char *s)
+{
+return ("%d", &s);
+}
