10ae2f6cf00c0295ee6c85d5b93ebfd48cc3b89f
Dukula <dutandrew78@gmail.com>
2022-10-24 22:05:21 -0700

    finding lengthvof a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,9 @@
  * _strlen- takes in a string of characters and return the string length
  * finding length of a string using strlen from string.h
  * @s: input parameter
- * len : returns the integer length of string of characters in s
+ * len - returns the integer length of string of characters in s
+ * Return: length of string
+ *
  */
 int _strlen(char *s)
 {
