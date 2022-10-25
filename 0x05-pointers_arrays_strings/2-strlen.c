8211201a4dfcf0ff94eeec49e71c45fe8525a225
Dukula <dutandrew78@gmail.com>
2022-10-24 21:29:25 -0700

    finding lengtybof a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
  * _strlen - takes in a string of characters and return the string length
+ * finding length of a string using strlen from string.h
  * @s: input parameter
  * return: the integer length of string of characters in s
  */
 int _strlen(char *s)
 {
-return ("%d", strlen(s));
+int len = ("%d", strlen(s));
+return (len);
 }
