96235442b18020975004792b4bfd1334cb37c79f
Dut Kulang <dutandrew78@gmail.com>
2022-11-02 00:37:58 +0300

    Done with task 5

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "main.h"
+void
+print_chessboard(char (*a)[8])
+{
+	int c, d;
+
+  /* Thinking of other ways to do this that don't use magic numbers */
+  
+	for (c = 0; c < 8; c++)
+	{
+		for (d = 0; d < 8; d++)
+		{
+			putchar(a[c][d]);
+		}
+		putchar('\n');
+	}
+}
