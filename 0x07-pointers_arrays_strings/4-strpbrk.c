32fcde62713b2ba5880a417b2d50ec50a11f4809
Dut Kulang <dutandrew78@gmail.com>
2022-11-02 00:33:15 +0300

    Done with project 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include "main.h"
+char *
+_strpbrk(char *s, char *accept)
+{
+	int c, j;
+
+	for (c = 0; s[c] > 0; c++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[c] == accept[j])
+				return (s + c);
+		}
+	}
+	return (NULL);
+}
