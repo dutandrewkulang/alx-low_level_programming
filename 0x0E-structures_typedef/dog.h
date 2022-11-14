c6e487e0ef0ebd4bf04b8c87e17ee7a1a10271a8
Dukula <dutandrew78@gmail.com>
2022-11-15 00:49:30 +0300

    The struct project done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,25 @@
-#ifndef DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - new type of struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
 
 struct dog
 {
-char * name;
-float * age;
-char * owner;
+	char *name;
+	float age;
+	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
-#endif
+#endif /*DOG_H*/
