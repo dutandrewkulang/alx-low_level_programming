11299eb4d339eb39df0a64f7c8925e9c55b4f64b
Dut <dutandrew78@gmail.com>
2023-08-16 19:58:57 +0300

    printing binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,15 +12,3 @@ void print_binary(unsigned long int n)
 	print_binary(n >> 1);
 	putchar((n & 1) + '0');
 }
-
-/**
- * print_binary - print the binary representation of a number `n'
- * @n: number to represent
- */
-void print_binary(unsigned long int n)
-{
-	if (n == 0)
-		_putchar('0');
-	else
-		print_binary(n);
-}
