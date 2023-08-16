9058cd4723e77e5a17c20438cb2e9fc23ec8e906
Dut <dutandrew78@gmail.com>
2023-08-16 18:56:43 +0300

    a function that checks for the endianness of processor

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks for endianness of processor
+ * Return: 0 on success
+ */
+
+int get_endianness(void){
+    unsigned int m = 1, *ptr;
+    ptr = &m;
+    return (*((char *) p));
+}
